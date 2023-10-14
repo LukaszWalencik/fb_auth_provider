4981cd72e89175719d3262d79be24b72988fbb7f
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-14 22:12:35 +0200

    Create User.fromDoc to use data from firebase in User model

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:equatable/equatable.dart';
 
 class User extends Equatable {
@@ -16,6 +17,16 @@ class User extends Equatable {
     required this.point,
     required this.rank,
   });
+  factory User.fromDoc(DocumentSnapshot userDoc) {
+    final userData = userDoc.data() as Map<String, dynamic>?;
+    return User(
+        id: userDoc.id,
+        name: userData!['name'],
+        email: userData['email'],
+        profileImage: userData['profileImage'],
+        point: userData['point'],
+        rank: userData['rank']);
+  }
 
   @override
   List<Object> get props {
