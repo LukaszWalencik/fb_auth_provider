631ad2611f094d93407f83ada32dd60e7366e6de
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-16 14:48:16 +0200

    Create simple SiginPage

---
+++ lib/pages/signin_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class SigninPage extends StatefulWidget {
+  const SigninPage({super.key});
+  static const String reouteName = '/signin';
+
+  @override
+  State<SigninPage> createState() => _SigninPageState();
+}
+
+class _SigninPageState extends State<SigninPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Signin'),
+      ),
+    );
+  }
+}
