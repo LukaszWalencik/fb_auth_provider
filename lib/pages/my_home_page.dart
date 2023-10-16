0f0c6528fef81165b4810740b8d6ef427506c3dd
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-16 14:54:11 +0200

    Change MyHomePage to stateful and add routeName

--- lib/pages/my_home_page.dart
+++ lib/pages/my_home_page.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+  static const String reouteName = '/home';
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Text('Home'),
+      ),
+    );
   }
 }
