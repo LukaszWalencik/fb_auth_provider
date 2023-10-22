3301ebeb18528becc41233edde42f6c2467b87a6
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-22 23:02:51 +0200

    Add MultiProvider and Provider of AuthRepository

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fb_auth_provider/pages/my_home_page.dart';
-import 'package:fb_auth_provider/pages/profile_page.dart';
 import 'package:fb_auth_provider/pages/signin_page.dart';
 import 'package:fb_auth_provider/pages/signup_page.dart';
 import 'package:fb_auth_provider/pages/splash_page.dart';
+import 'package:fb_auth_provider/repositories/auth_repository.dart';
+import 'package:firebase_auth/firebase_auth.dart' as fbAuth;
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
+import 'package:provider/provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,18 +24,27 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Auth Provider',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        Provider<AuthRepository>(
+          create: (context) => AuthRepository(
+              firebaseFirestore: FirebaseFirestore.instance,
+              firebaseAuth: fbAuth.FirebaseAuth.instance),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Auth Provider',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: SplashPage(),
+        routes: {
+          SignupPage.routeName: (context) => SignupPage(),
+          SigninPage.routeName: (context) => SigninPage(),
+          MyHomePage.routeName: (context) => MyHomePage(),
+        },
       ),
-      home: SplashPage(),
-      routes: {
-        SignupPage.routeName: (context) => SignupPage(),
-        SigninPage.routeName: (context) => SigninPage(),
-        MyHomePage.routeName: (context) => MyHomePage(),
-      },
     );
   }
 }
