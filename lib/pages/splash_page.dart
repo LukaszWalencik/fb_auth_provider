8ddf2f78a50fb9c69bf289efa2bc07e922896be2
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-22 23:28:13 +0200

    Inject authState if statement to check user is login or not

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,16 +1,29 @@
+import 'package:fb_auth_provider/pages/my_home_page.dart';
+import 'package:fb_auth_provider/pages/signin_page.dart';
+import 'package:fb_auth_provider/pages/signup_page.dart';
+import 'package:fb_auth_provider/providers/auth/auth_provider.dart';
+import 'package:fb_auth_provider/providers/auth/auth_state.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class SplashPage extends StatefulWidget {
+class SplashPage extends StatelessWidget {
   const SplashPage({super.key});
   static const String routeName = '/';
 
   @override
-  State<SplashPage> createState() => _SplashPageState();
-}
-
-class _SplashPageState extends State<SplashPage> {
-  @override
   Widget build(BuildContext context) {
+    final authState = context.watch<AuthProvider>().state;
+    if (authState == AuthStatus.unauthenticated) {
+      WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => SignupPage()));
+      });
+    } else if (authState == AuthStatus.authenticated) {
+      WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => MyHomePage()));
+      });
+    }
     return Center(
       child: CircularProgressIndicator(),
     );
