9fb63f5e695efd04999b20c7dfb64fedda0991a3
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-23 22:08:14 +0200

    Create SigninProvider class with state geter and AuthRepository final variable

--- lib/providers/signin/signin_provider.dart
+++ lib/providers/signin/signin_provider.dart
@@ -1,1 +1,12 @@
+import 'package:fb_auth_provider/providers/signin/signin_state.dart';
+import 'package:fb_auth_provider/repositories/auth_repository.dart';
+import 'package:flutter/material.dart';
 
+class SigninProvider with ChangeNotifier {
+  SignInState _state = SignInState.initial();
+
+  SigninProvider({required this.authRepository});
+  SignInState get state => _state;
+
+  final AuthRepository authRepository;
+}
