ba10d45853f2ccf031fe7e9cef4a80bf82fe8eb0
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-23 22:14:44 +0200

    Create signin function

--- lib/providers/signin/signin_provider.dart
+++ lib/providers/signin/signin_provider.dart
@@ -1,3 +1,4 @@
+import 'package:fb_auth_provider/models/custom_error.dart';
 import 'package:fb_auth_provider/providers/signin/signin_state.dart';
 import 'package:fb_auth_provider/repositories/auth_repository.dart';
 import 'package:flutter/material.dart';
@@ -9,4 +10,18 @@ class SigninProvider with ChangeNotifier {
   SignInState get state => _state;
 
   final AuthRepository authRepository;
+
+  Future<void> signin({required String email, required String password}) async {
+    _state = _state.copyWith(signInStatus: SignInStatus.submitting);
+    notifyListeners();
+    try {
+      await authRepository.signin(email: email, password: password);
+      _state = _state.copyWith(signInStatus: SignInStatus.success);
+      notifyListeners();
+    } on CustomError catch (e) {
+      _state = _state.copyWith(signInStatus: SignInStatus.error, error: e);
+      notifyListeners();
+      rethrow;
+    }
+  }
 }
