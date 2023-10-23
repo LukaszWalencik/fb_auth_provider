96682599c2227d94f96aa8c686c4f1558fc54e29
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-23 22:05:34 +0200

    Create sigin_state dart file and cut all of code from provider file into state

---
+++ lib/providers/signin/signin_state.dart
@@ -0,0 +1,34 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+import 'package:fb_auth_provider/models/custom_error.dart';
+
+enum SignInStatus { initial, submitting, success, error }
+
+class SignInState extends Equatable {
+  final SignInStatus signInStatus;
+  final CustomError error;
+
+  SignInState({required this.signInStatus, required this.error});
+
+  factory SignInState.initial() {
+    return SignInState(
+        signInStatus: SignInStatus.initial, error: CustomError());
+  }
+
+  @override
+  List<Object> get props => [signInStatus, error];
+
+  @override
+  bool get stringify => true;
+
+  SignInState copyWith({
+    SignInStatus? signInStatus,
+    CustomError? error,
+  }) {
+    return SignInState(
+      signInStatus: signInStatus ?? this.signInStatus,
+      error: error ?? this.error,
+    );
+  }
+}
