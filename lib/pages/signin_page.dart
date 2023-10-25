ad379f8f9b3391005797f316777e26c02265a502
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-25 20:34:20 +0200

    Add GlobalKey<FormState> as _formKey, autovalidateMode as diabled and nullable value email and password to Signin page

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:validators/validators.dart';
 
 class SigninPage extends StatefulWidget {
   const SigninPage({super.key});
@@ -9,11 +10,28 @@ class SigninPage extends StatefulWidget {
 }
 
 class _SigninPageState extends State<SigninPage> {
+  final _formKey = GlobalKey<FormState>();
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+  String? _email, _password;
+
+  void submit() {}
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Signin'),
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Form(
+                key: _formKey,
+                autovalidateMode: _autovalidateMode,
+                child: ListView(
+                  shrinkWrap: false,
+                  children: [],
+                )),
+          ),
+        ),
       ),
     );
   }
