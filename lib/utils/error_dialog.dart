e560e17fe1707204e7e8f0d91eed8daf189850c2
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-26 21:53:30 +0200

    Create errorDialog function to show dialog depense what Platform user use, now have done for IOS

---
+++ lib/utils/error_dialog.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+import 'package:fb_auth_provider/models/custom_error.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void errorDialog(BuildContext context, CustomError e) {
+  if (Platform.isIOS) {
+    showCupertinoDialog(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: Text(e.code),
+          content: Text(e.plugin + '\n' + e.message),
+          actions: [
+            CupertinoDialogAction(
+              child: Text('OK'),
+              onPressed: () => Navigator.pop(context),
+            )
+          ],
+        );
+      },
+    );
+  }
+}
