751bd3e7b70b61a2ca2086fb2b7417ceaa5c704d
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-10-30 21:58:01 +0100

    Create SignUpPage similar as SignInPage, create one more TextFormField and use TextEditingController to check text in both password Fields are the same or not match

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,19 +1,174 @@
+import 'package:fb_auth_provider/models/custom_error.dart';
+import 'package:fb_auth_provider/providers/signup/signup_provider.dart';
+import 'package:fb_auth_provider/providers/signup/signup_state.dart';
+import 'package:fb_auth_provider/utils/error_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:validators/validators.dart';
 
-class SignupPage extends StatefulWidget {
-  const SignupPage({super.key});
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
   static const String routeName = '/signup';
 
   @override
-  State<SignupPage> createState() => _SignupPageState();
+  State<SignUpPage> createState() => _SignUpPageState();
 }
 
-class _SignupPageState extends State<SignupPage> {
+class _SignUpPageState extends State<SignUpPage> {
+  final _formKey = GlobalKey<FormState>();
+  AutovalidateMode _autovalidateMode = AutovalidateMode.disabled;
+  String? _name, _email, _password;
+  final _passwordController = TextEditingController();
+
+  void _submit() async {
+    setState(() {
+      _autovalidateMode = AutovalidateMode.always;
+    });
+    final form = _formKey.currentState;
+    if (form == null || !form.validate()) return;
+    print('Name:c$_name Email: $_email  Password: $_password');
+    form.save();
+    try {
+      await context
+          .read<SignUpProvider>()
+          .signup(name: _name!, email: _email!, password: _password!);
+    } on CustomError catch (e) {
+      errorDialog(context, e);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text('Signup'),
+    final signupState = context.watch<SignUpProvider>().state;
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        body: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 30),
+            child: Form(
+                key: _formKey,
+                autovalidateMode: _autovalidateMode,
+                child: ListView(
+                  shrinkWrap: false,
+                  children: [
+                    TextFormField(
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          filled: true,
+                          labelText: 'Name',
+                          prefixIcon: Icon(Icons.account_box)),
+                      validator: (String? value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Name required';
+                        }
+                        if (value.trim().length < 2) {
+                          return 'Name must be at least 2 characters long';
+                        }
+                        return null;
+                      },
+                      onSaved: (String? value) {
+                        _name = value;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      keyboardType: TextInputType.emailAddress,
+                      autocorrect: false,
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(),
+                          filled: true,
+                          labelText: 'Email',
+                          prefixIcon: Icon(Icons.email)),
+                      validator: (String? value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Email required';
+                        }
+                        if (!isEmail(value.trim())) {
+                          return 'Enter a valid email';
+                        }
+                        return null;
+                      },
+                      onSaved: (String? value) {
+                        _email = value;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        labelText: 'Password',
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                      validator: (String? value) {
+                        if (value == null || value.trim().isEmpty) {
+                          return 'Password is required';
+                        }
+                        if (value.trim().length < 6) {
+                          return 'Password must be at least 6 characters long';
+                        }
+                        return null;
+                      },
+                      onSaved: (String? value) {
+                        _password = value;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        filled: true,
+                        labelText: 'Conform password',
+                        prefixIcon: Icon(Icons.lock),
+                      ),
+                      validator: (String? value) {
+                        if (value != _passwordController.text) {
+                          return 'Password not match ';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed:
+                          signupState.signUpStatus == SignUpStatus.submitting
+                              ? null
+                              : _submit,
+                      child: Text(
+                          signupState.signUpStatus == SignUpStatus.submitting
+                              ? 'Loading..'
+                              : 'Sign Up'),
+                      style: ElevatedButton.styleFrom(
+                        textStyle: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                        padding: EdgeInsets.symmetric(vertical: 10),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    TextButton(
+                      onPressed:
+                          signupState.signUpStatus == SignUpStatus.submitting
+                              ? null
+                              : () {
+                                  Navigator.pop(context);
+                                },
+                      child: Text('Already a member? Sign up!'),
+                      style: TextButton.styleFrom(
+                        textStyle: TextStyle(
+                            fontSize: 30, decoration: TextDecoration.underline),
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ),
       ),
     );
   }
