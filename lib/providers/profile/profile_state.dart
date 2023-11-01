e4653ba5256662b84f943099a41bbb0fc9f08ead
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-01 19:50:26 +0100

    Create ProfilState using ProfilStatus, User and CustomError, create it qith Equatable, toString, copyWith

--- lib/providers/profile/profile_state.dart
+++ lib/providers/profile/profile_state.dart
@@ -1,6 +1,41 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:equatable/equatable.dart';
+
+import 'package:fb_auth_provider/models/custom_error.dart';
+import 'package:fb_auth_provider/models/user_model.dart';
+
 enum ProfileStatus {
   initial,
   loading,
   loaded,
   error,
+}
+
+class ProfileState extends Equatable {
+  final ProfileStatus profileStatus;
+  final User user;
+  final CustomError customError;
+  ProfileState({
+    required this.profileStatus,
+    required this.user,
+    required this.customError,
+  });
+
+  @override
+  List<Object> get props => [profileStatus, user, customError];
+
+  @override
+  bool get stringify => true;
+
+  ProfileState copyWith({
+    ProfileStatus? profileStatus,
+    User? user,
+    CustomError? customError,
+  }) {
+    return ProfileState(
+      profileStatus: profileStatus ?? this.profileStatus,
+      user: user ?? this.user,
+      customError: customError ?? this.customError,
+    );
+  }
 }
