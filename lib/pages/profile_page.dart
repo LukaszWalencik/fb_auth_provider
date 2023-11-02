d0d0c9790600a2688f54025e31ff32441921f7d9
Lukasz Walencik <walencik.lukasz@gmail.com>
2023-11-02 21:45:33 +0100

    Add late final ProfileProvider value and use it in initial state to get data or emit error (using listener)

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,4 +1,8 @@
+import 'package:fb_auth_provider/providers/profile/profile_provider.dart';
+import 'package:fb_auth_provider/providers/profile/profile_state.dart';
+import 'package:fb_auth_provider/utils/error_dialog.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
@@ -8,6 +12,28 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  late final ProfileProvider profilProvider;
+
+  @override
+  void initState() {
+    profilProvider = context.read<ProfileProvider>();
+    profilProvider.addListener(errorDialogListener);
+
+    super.initState();
+  }
+
+  void errorDialogListener() {
+    if (profilProvider.state.profileStatus == ProfileStatus.error) {
+      errorDialog(context, profilProvider.state.customError);
+    }
+  }
+
+  @override
+  void dispose() {
+    profilProvider.removeListener(errorDialogListener);
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
